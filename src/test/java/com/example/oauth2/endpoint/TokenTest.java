ae0835560877295d9a32dd69ab69b4ee7ccb1444
大胃王 <253498229@qq.com>
2018-12-10 16:45:24 +0800

    polish

--- src/test/java/com/example/oauth2/endpoint/TokenTest.java
+++ src/test/java/com/example/oauth2/endpoint/TokenTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class TokenTest {
     @Resource
     private MockMvc mockMvc;
